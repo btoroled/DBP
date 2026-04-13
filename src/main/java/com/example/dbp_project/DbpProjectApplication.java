990b50010c8931359a4fbcd829d1b4fe88fd670b
elbull-tarhur <benja.toro.l98@gmail.com>
2026-04-13 11:59:26 -0500

    Creacion de proyecto en JAVA 26, con framework de Springboot y LOMBOK

---
+++ src/main/java/com/example/dbp_project/DbpProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.dbp_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbpProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DbpProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/dbp_project/DbpProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dbp_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbpProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
