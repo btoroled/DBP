64f9bf108cf8e6b95a03a70e05fe8771a99bed53
elbull-tarhur <benja.toro.l98@gmail.com>
2026-05-02 10:39:09 -0500

    Creacion del proyecto y configuracion de pom.xml

--- src/main/java/com/streakstudy/StreakStudyApplication.java
+++ src/main/java/com/streakstudy/StreakStudyApplication.java
@@ -1,13 +1,11 @@
-package com.streakstudy.dbp_project.;
+package com.streakstudy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class StreakStudyApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(StreakStudyApplication.class, args);
     }
-
 }
--- src/test/java/com/streakstudy/StreakStudyApplicationTests.java
+++ src/test/java/com/streakstudy/StreakStudyApplicationTests.java
@@ -1,13 +1,19 @@
-package com.streakstudy.dbp_project;
+package com.streakstudy;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource(properties = {
+        "spring.datasource.url=",
+        "spring.jpa.hibernate.ddl-auto=none",
+        "spring.autoconfigure.exclude=org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration," +
+                "org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration"
+})
 class StreakStudyApplicationTests {
 
     @Test
     void contextLoads() {
     }
-
 }
